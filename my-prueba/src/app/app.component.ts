82931ba41295fde1c5769a291734bd2a9b436970
Alexandra565 <tbetancurth@unal.edu.co>
2024-03-28 14:58:57 -0500

    se crea la seccion del banner, con sus 3 imagenes responsive

--- my-prueba/src/app/app.component.ts
+++ my-prueba/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { SectionBodyComponent } from './section-body/section-body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,BodyComponent],
+  imports: [RouterOutlet,HeaderComponent,BodyComponent,SectionBodyComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ my-prueba/src/app/section-body/section-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-body.component.html',
+  styleUrl: './section-body.component.scss'
+})
+export class SectionBodyComponent {
+
+}
